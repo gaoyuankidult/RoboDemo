61af8b6b2d42cb863acfa82e0d6f86da3351f2a8
Yuan Gao <gaoyuankidult@gmail.com>
2017-08-24 16:05:21 +0200

    Updated the install.sh. fixed some problems.

---
+++ install.sh
@@ -0,0 +1,4 @@
+pip install SpeechRecognition
+
+sudo apt-get install portaudio19-dev
+pip install PyAudio
--- speech_mimic.py
+++ speech_mimic.py
@@ -20,11 +20,12 @@ while True:
             text = str(r.recognize_google(audio))
             print type(text)
             print("Google Speech Recognition thinks you said " + text)
+            tts.say(text)
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
-        tts.say(text)
+
     else:
         print("Please choice between 1 and 2")
         choice = raw_input("text(1) or speech(2)")
